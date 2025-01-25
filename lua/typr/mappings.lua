aa2c456bc4f2adec5ca639e353eedfd594a4a77e
siduck <siduck@tutanota.com>
2025-01-25 15:45:28 +0530

    fix: handle next-line cursor within set_lines callback itself

--- lua/typr/mappings.lua
+++ lua/typr/mappings.lua
@@ -5,22 +5,6 @@ local myapi = require "typr.api"
 local utils = require "typr.utils"
 
 return function()
-  map("i", "<Space>", function()
-    local pos = vim.api.nvim_win_get_cursor(state.win)
-    local curline_end = #state.default_lines[pos[1] - state.words_row]
-
-    if pos[2] > curline_end then
-      if state.words_row_end == pos[1] then
-        utils.on_finish()
-        return
-      end
-
-      api.nvim_win_set_cursor(state.win, { pos[1] + 1, state.xpad })
-    else
-      api.nvim_feedkeys(" ", "n", true)
-    end
-  end, { buffer = state.buf })
-
   map("n", "i", function()
     api.nvim_win_set_cursor(state.win, { state.words_row + 1, state.xpad })
     vim.cmd.startinsert()
